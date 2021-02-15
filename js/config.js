49bc1acab098706aa99b085e5d19f2a1d797afc4
mrbrandonyi <77752802+mrbrandonyi@users.noreply.github.com>
2021-02-15 12:28:39 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-2_QyHQYxEgY', // us-east-2_QyHQYxEgY
-        userPoolClientId: 'd5o54t5347ichgt6e7bilr2bp', // e.g. d5o54t5347ichgt6e7bilr2bp
-        region: 'us-east-2' // e.g. us-east-2
+        userPoolId: 'us-east-2_QyHQYxEgY', 
+        userPoolClientId: 'd5o54t5347ichgt6e7bilr2bp', 
+        region: 'us-east-2' 
     },
     api: {
-        invokeUrl: 'https://d2erypcs5rtwi1.execute-api.us-east-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://jzxclwlzx5.execute-api.us-east-2.amazonaws.com/prod'
     }
 };
