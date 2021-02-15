7d5dc2c14e5d8a3bec03b7e0c85b6b4901b90bc8
mrbrandonyi <77752802+mrbrandonyi@users.noreply.github.com>
2021-02-15 11:55:19 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: '', // us-east-2_QyHQYxEgY
-        userPoolClientId: '', // e.g. d5o54t5347ichgt6e7bilr2bp
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-2_QyHQYxEgY', // us-east-2_QyHQYxEgY
+        userPoolClientId: 'd5o54t5347ichgt6e7bilr2bp', // e.g. d5o54t5347ichgt6e7bilr2bp
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://d2erypcs5rtwi1.execute-api.us-east-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
